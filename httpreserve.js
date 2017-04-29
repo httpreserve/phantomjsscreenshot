d39373fbda18cb1aff8a18335392bef5de6a2514
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-04-29 18:15:50 +1200

    [feature] add user agent.

--- httpreserve.js
+++ httpreserve.js
@@ -11,9 +11,13 @@ address = system.args[1];
 output = system.args[2];
 
 var page = require('webpage').create();
+
 //viewportSize being the actual size of the headless browser
 page.viewportSize = { width: 1024, height: 768 };
 page.clipRect = { left: 0, top: 0, width: 1024, height: 768 };
+
+//set a sensible user-agent
+page.settings.userAgent = 'httpreserve-websnapshot-0.0.1';
 
 //the rest of the code is the same as the previous example
 page.open(address, function() {
